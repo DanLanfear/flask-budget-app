2bc0230fc7e70a9b5b8a5792fd2ad27dfe28ce08
DanLanfear <lanfeadb@udmercy.edu>
2024-09-29 19:19:25 -0400

    Added dummy data for home route

--- flask-budget/budget.py
+++ flask-budget/budget.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
+transactions = [
+    {
+        'date': 'September 29, 2024',
+        'name': 'kroger',
+        'amount': 109.84,
+    },
+    {
+        'date': 'September 30, 2024',
+        'name': 'gas',
+        'amount': 30.67,
+    }
+]
+
+
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', transactions=transactions)
 
 
 

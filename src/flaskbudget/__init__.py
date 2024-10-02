89cdc21f1a67f99bd5e0121bbf01331863d74af1
DanLanfear <lanfeadb@udmercy.edu>
2024-10-01 22:08:54 -0400

    removed basedir for database uri

--- src/flaskbudget/__init__.py
+++ src/flaskbudget/__init__.py
@@ -1,9 +1,5 @@
-import os   
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1da3698b08a11964197e690e97054059'
